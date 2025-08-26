de30aa39baa8644411f29092848d2c88fbdf009c
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2025-08-26 18:05:33 -0400

    Initial Commit

---
+++ H4X0R News/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by Lidiia Diachkovskaia on 8/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
